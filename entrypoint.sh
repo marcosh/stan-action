ebf01e9bb52b9676dd5acf14b0f22f4ef48488eb
Marco Perone <pasafama@gmail.com>
2021-06-22 12:52:45 +0200

    try using ghv and stan versions as inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 ls -a
-curl --silent -L https://github.com/kowainik/stan/releases/latest/download/stan-0.0.1.0-Linux-ghc-8.8.3 --output stan
+curl --silent -L https://github.com/kowainik/stan/releases/latest/download/stan-$2-Linux-ghc-$1 --output stan
 
 echo "Pwd: $(pwd)"
 echo "::set-output name=stanPath::$(pwd)/stan"
